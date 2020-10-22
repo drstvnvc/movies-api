dbd91dabd2f0c94d3980f8156a01042bf4bf400b
Drago Stevanovic <drago.s@vivifyideas.com>
2020-10-22 18:40:12 +0200

    Add filtering, sorting and pagination

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateMovieRequest;
+use App\Http\Requests\UpdateMovieRequest;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,35 @@ class MoviesController extends Controller
    *
    * @return \Illuminate\Http\Response
    */
-  public function index()
+  public function index(Request $request)
   {
-    $movies = Movie::all();
+    
+    \DB::listen(function ($query) {
+      info($query->sql);
+    });
+
+    // $page_size = $request->get('page_size');
+    $moviesQuery = Movie::query();
+
+    $title = $request->get('title', '');
+    if ($title) {
+      $moviesQuery->where('title', 'like', '%' . $title . '%');
+    }
+
+    $orderBy = $request->get('orderBy', null);
+    $orderDirection = $request->get('orderDir', 'asc');
+    if ($orderBy) {
+      $moviesQuery->orderBy($orderBy, $orderDirection);
+    }
+
+
+    $take = $request->get('take', null);
+    $skip = $request->get('skip', 0);
+    if ($take) {
+      $moviesQuery->skip($skip)->take($take);
+    }
+    
+    $movies = $moviesQuery->paginate();
     return response()->json($movies);
   }
 
@@ -58,9 +85,11 @@ class MoviesController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $id)
+  public function update(UpdateMovieRequest $request, $id)
   {
-    //
+    $movie = Movie::findOrFail($id);
+    $movie->update($request->validated());
+    return $movie;
   }
 
   /**
@@ -71,6 +100,8 @@ class MoviesController extends Controller
    */
   public function destroy($id)
   {
-    //
+    $movie = Movie::findOrFail($id);
+    $movie->delete();
+    return $movie;
   }
 }
